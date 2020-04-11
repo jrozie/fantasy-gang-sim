6631c71aa8843f38ad3567f7f4d2453b35e987f2
jrozie <jackcatsbridge@gmail.com>
2020-04-11 15:09:48 +0100

    3

--- Gangs/main.py
+++ Gangs/main.py
@@ -1,6 +1,9 @@
 '''
 Scoping Doc:
 https://docs.google.com/document/d/1R7wLJT4Uw3O5aAvmx0tQjIShlIkPdfamBE8NFfvebM8/edit?usp=sharing
+
+Github:
+https://github.com/jrozie/fantasy-gang-sim
 '''
 # %%
 
